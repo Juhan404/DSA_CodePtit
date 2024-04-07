b5357f46a92850352e1cb288d6e209ed0babd4e4
sinhviennam8 <phamchinhhoang@gmail.com>
2024-04-07 17:05:45 +0700

    Code

--- Bài thực hành 2 Thầy Tiến/VUA VÀ QUÂN MÃ.cpp
+++ Bài thực hành 2 Thầy Tiến/VUA VÀ QUÂN MÃ.cpp
@@ -22,11 +22,11 @@ using pii = pair<int, int>;
 const int MOD = 1e9 + 7;
 const int N = 1e5 + 5;
 
-int n;
-char a[105][105];
-int vsv[105][105], vsm[105][105], v[105][105], m[105][105];
-pii vua, ma;
-int res = 1e9;
+char c;
+int n, res = 1e9;
+int king_vs[105][105], knight_vs[105][105];
+int king[105][105], knight[105][105];
+pii king_pos, knight_pos;
 
 int dx1[] = {0, 0, 1, -1, 1, -1, 1, -1};
 int dy1[] = {1, -1, 0, 0, 1, -1, -1, 1};
@@ -34,12 +34,12 @@ int dy1[] = {1, -1, 0, 0, 1, -1, -1, 1};
 int dx2[] = {-2, -1, 1, 2, 2, 1, -1, -2};
 int dy2[] = {-1, -2, -2, -1, 1, 2, 2, 1};
 
-void BFS_vua(int x, int y) {
+void BFS_king(int x, int y) {
     queue<pii> q;
 
     q.push({x, y});
-    vsv[x][y] = 1;
-    v[x][y] = 0;
+    king_vs[x][y] = 1;
+    king[x][y] = 0;
 
     while (!q.empty()) {
         pii temp = q.front();
@@ -49,38 +49,36 @@ void BFS_vua(int x, int y) {
             int x1 = temp.fi + dx1[i];
             int y1 = temp.se + dy1[i];
 
-            if (x1 >= 1 && x1 <= n && y1 >= 1 && y1 <= n && !vsv[x1][y1]) {
-                vsv[x1][y1] = 1;
-                v[x1][y1] = v[temp.fi][temp.se] + 1;
+            if (x1 >= 1 && x1 <= n && y1 >= 1 && y1 <= n && !king_vs[x1][y1]) {
+                king_vs[x1][y1] = 1;
+                king[x1][y1] = king[temp.fi][temp.se] + 1;
                 q.push({x1, y1});
             }
         }
     }
 }
 
-void BFS_ma(int x, int y) {
+void BFS_knight(int x, int y) {
     queue<pii> q;
 
     q.push({x, y});
-    vsm[x][y] = 1;
-    m[x][y] = 0;
+    knight_vs[x][y] = 1;
+    knight[x][y] = 0;
 
     while (!q.empty()) {
         pii temp = q.front();
         q.pop();
 
-        if (v[temp.fi][temp.se] == m[temp.fi][temp.se]) {
-            res = min(res, m[temp.fi][temp.se]);
-            debug(temp.fi, temp.se);
-        }
+        if (king[temp.fi][temp.se] == knight[temp.fi][temp.se])
+            res = min(res, king[temp.fi][temp.se]);
 
         FOR(i, 0, 7) {
             int x1 = temp.fi + dx2[i];
             int y1 = temp.se + dy2[i];
 
-            if (x1 >= 1 && x1 <= n && y1 >= 1 && y1 <= n && !vsm[x1][y1]) {
-                vsm[x1][y1] = 1;
-                m[x1][y1] = m[temp.fi][temp.se] + 1;
+            if (x1 >= 1 && x1 <= n && y1 >= 1 && y1 <= n && !knight_vs[x1][y1]) {
+                knight_vs[x1][y1] = 1;
+                knight[x1][y1] = knight[temp.fi][temp.se] + 1;
                 q.push({x1, y1});
             }
         }
@@ -89,28 +87,28 @@ void BFS_ma(int x, int y) {
 
 void run_case() {
     cin >> n;
-    char c;
+
     FOR(i, 1, n) {
         FOR(j, 1, n) {
             cin >> c;
 
-            v[i][j] = 1e9;
-            m[i][j] = 1e9;
+            king[i][j] = 1e9;
+            knight[i][j] = 1e9;
 
             if (c == '#') {
-                vsv[i][j] = 1;
-                vsm[i][j] = 1;
+                king_vs[i][j] = 1;
+                knight_vs[i][j] = 1;
             }
 
             if (c == 'M')
-                ma = {i, j};
+                knight_pos = {i, j};
             else if (c == 'T')
-                vua = {i, j};
+                king_pos = {i, j};
         }
     }
 
-    BFS_vua(vua.fi, vua.se);
-    BFS_ma(ma.fi, ma.se);
+    BFS_king(king_pos.fi, king_pos.se);
+    BFS_knight(knight_pos.fi, knight_pos.se);
 
     if (res == 1e9)
         cout << "-1" << endl;
